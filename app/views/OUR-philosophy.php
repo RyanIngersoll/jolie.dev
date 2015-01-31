0feb43ff233ed158658842806674cff5affa21c2
Ryan Ingersoll <ryan@sridiversified.com>
2015-01-30 21:24:43 -0600

    started dads new webpage

---
+++ app/views/OUR-philosophy.php
@@ -0,0 +1,23 @@
+OUR PHILOSOPHY
+
+SRI DIVERSIFIED CAPITAL, LLC. is a leading risk management consultant for institutional  and high-net-worth investors who are seeking to diversify their portfolios from the growing risks in today's capital markets.  Our top priority is helping our clients achieve their goals and needs while reducing exposure to the inflation and interest rate risks inherent in the market. 
+
+SRI has forged strategic relationships with some of the best independent drillers in the South Texas Eagle Ford Shale development, which enables us to provide an exclusive gateway into a valuable alternative asset class for our clients.
+
+
+
+
+
+A Letter From the President and CEO, Frank Ingersoll
+
+As a consultant to high net worth investors, it is my fiduciary responsibility to make sure you are receiving sound wisdom and education as the foundation for managing your wealth.  This means understanding the true meaning of risk diversification when it comes to the Eagle Ford.  I am not just talking about diversification among various asset classes in the traditional sense.  The most critical aspect that many don't yet understand is that to be truly diversified is to build protection against the two biggest enemies of wealth building, inflation and taxes. 
+
+My desire is to help you build a defensive portfolio against these elements to grow your wealth through the incredible benefits allocated to oil and gas drilling interests.  I have developed this website to further help you understand how the game has changed from the old wildcatting days of vertical drilling.  You will see how horizontal drilling has transformed the Eagle Ford into a low risk investment that can dramatically improve portfolio performance.  Please enjoy the content and feel free to contact me for more information. 
+
+
+
+Sincerely,
+
+ 
+
+Frank (Sandy) Ingersoll
---
+++ app/views/wealthtec.php
@@ -0,0 +1,9 @@
+WealthTEC Strategies specializes in quantitative money management for high net-worth investors.  
+
+Mission:  Risk adjusted outperformance of the major asset class benchmarks - The S&P 500 and Nasdaq 100 Indices and the 10-Year U.S. Treasury Yield. 
+
+“The contrary investor resigns himself from the herd and thinks for himself”
+— Archibald MacLeish
+At WealthTEC, we meticulously identify unique short-term opportunities in equities and commodities to put cash to work.  We don't chase momentum or catch falling knives and every trade has a defined level of risk per unit of return.  We thrive on maintaining the liquidity and flexibility to always be a step ahead of the herd.   
+
+If you are tired of earning a negative real return from your current "safe" cash and savings plans offered by most advisors, please contact us to make a fresh start on your financial future. 
